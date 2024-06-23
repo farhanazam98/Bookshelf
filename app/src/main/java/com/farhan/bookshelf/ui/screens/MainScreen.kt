8c32db642f2a571a4a033cbf3d9ac65efaa767b0
Farhan Azam <farhanazam@meta.com>
2024-06-23 19:51:21 -0400

    Add placeholder image

--- app/src/main/java/com/farhan/bookshelf/ui/screens/MainScreen.kt
+++ app/src/main/java/com/farhan/bookshelf/ui/screens/MainScreen.kt
@@ -1,6 +1,6 @@
 package com.farhan.bookshelf.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,37 +11,53 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.farhan.bookshelf.model.BookItem
+import com.farhan.bookshelf.R
+import com.farhan.bookshelf.model.ImageLinks
 import com.farhan.bookshelf.model.VolumeInfo
 import com.farhan.bookshelf.ui.theme.BookshelfTheme
 
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier, books : List<VolumeInfo>){
+fun MainScreen(modifier: Modifier = Modifier, books: List<VolumeInfo>) {
     LazyColumn(modifier = modifier) {
-        items(books){
-            it: VolumeInfo -> BookCard(bookInfo = it)
+        items(books) { it: VolumeInfo ->
+            BookCard(bookInfo = it)
         }
     }
 }
 
 @Composable
-fun BookCard(bookInfo: VolumeInfo, modifier: Modifier = Modifier) {
+fun BookCard(
+    bookInfo: VolumeInfo,
+    @DrawableRes placeholder: Int = R.drawable.loading_img,
+) {
     val authorsString = bookInfo.authors.joinToString(", \n")
-    val resolvedUrl = if (bookInfo.imageLinks != null) bookInfo.imageLinks.thumbnail.replace("http", "https") else "https://fakeimg.pl/200x250?text=No+cover&font=bebas"
+    val resolvedUrl: String =
+        if (bookInfo.imageLinks != null) bookInfo.imageLinks.thumbnail.replace(
+            "http",
+            "https"
+        ) else "https://fakeimg.pl/200x250?text=No+cover&font=bebas"
     Card {
-        Column(modifier = modifier.padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-            AsyncImage(model = resolvedUrl, contentDescription = null)
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = resolvedUrl,
+                contentDescription = null,
+                placeholder = painterResource(placeholder)
+            )
             Text(
                 text = "Title: ${bookInfo.title}",
                 style = MaterialTheme.typography.labelMedium,
             )
             Text(
-                text = "Author(s): ${authorsString}",
+                text = "Author(s): $authorsString",
                 style = MaterialTheme.typography.labelSmall,
                 textAlign = TextAlign.Center
 
@@ -52,10 +68,18 @@ fun BookCard(bookInfo: VolumeInfo, modifier: Modifier = Modifier) {
 }
 
 
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BookCardPreview() {
+    val imageLinks = ImageLinks(thumbnail = PLACEHOLDER_URL)
+    val bookInfo = VolumeInfo(
+        title = "Moby Dick",
+        authors = listOf("Herman Melville"),
+        imageLinks = imageLinks
+    )
     BookshelfTheme {
+        BookCard(bookInfo = bookInfo, placeholder = R.drawable.moby_dick)
     }
 }
+
+const val PLACEHOLDER_URL = "https://fakeimg.pl/200x250?text=No+cover&font=bebas"

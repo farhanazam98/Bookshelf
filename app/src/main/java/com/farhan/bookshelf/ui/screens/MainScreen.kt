86557a6d28907b599674e39a9ac93c137cad9023
farhanazam98 <farhanazam98@yahoo.com>
2024-06-30 08:57:50 -0400

    Display results in LazyVerticalStaggeredGrid

--- app/src/main/java/com/farhan/bookshelf/ui/screens/MainScreen.kt
+++ app/src/main/java/com/farhan/bookshelf/ui/screens/MainScreen.kt
@@ -7,13 +7,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,76 +33,39 @@ import com.farhan.bookshelf.ui.theme.BookshelfTheme
 
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier, bookImageUrls: List<String>) {
-    LazyColumn(modifier = modifier) {
+fun MainScreen(modifier: Modifier = Modifier, bookImageUrls: List<String>, @DrawableRes placeholder: Int =  R.drawable.loading_img) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        modifier = modifier) {
         items(bookImageUrls) { it: String ->
-//            BookCard(bookInfo = it)
-            Book(it)
+            Book(it, placeholder)
         }
     }
 }
 
 @Composable
 fun Book(imageUrl: String,
-         @DrawableRes placeholder: Int = R.drawable.loading_img,
+         @DrawableRes placeholder: Int,
 ) {
     val formattedUrl = imageUrl.replace("http", "https")
     AsyncImage(
         model = formattedUrl,
         contentDescription = null,
-        placeholder = painterResource(placeholder)
+        placeholder = painterResource(placeholder),
+        contentScale = ContentScale.FillWidth
+
     )
 }
-
-@Composable
-fun BookCard(
-    bookInfo: VolumeInfo,
-    @DrawableRes placeholder: Int = R.drawable.loading_img,
-) {
-    val authorsString = bookInfo.authors.joinToString(", \n")
-    // remove assertion
-    val resolvedUrl: String =
-        if (bookInfo.imageLinks != null) bookInfo.imageLinks.thumbnail?.replace(
-            "http",
-            "https"
-        )!! else "https://fakeimg.pl/200x250?text=No+cover&font=bebas"
-    Card {
-        Row(
-            modifier = Modifier.padding(8.dp).fillMaxWidth(),
-        ) {
-            AsyncImage(
-                model = resolvedUrl,
-                contentDescription = null,
-                placeholder = painterResource(placeholder)
-            )
-            Column(modifier = Modifier.padding(all = 4.dp)){
-                Text(
-                    text = "Title:\n${bookInfo.title}",
-                    style = MaterialTheme.typography.labelMedium,
-                )
-                Text(
-                    text = "Author(s): \n$authorsString",
-                    style = MaterialTheme.typography.labelSmall,
-                )
-            }
-        }
-    }
-
-}
-
 
 @Preview(showBackground = true)
 @Composable
-fun BookCardPreview() {
-    val imageLinks = ImageLinks(thumbnail = PLACEHOLDER_URL)
-    val bookInfo = VolumeInfo(
-        title = "Moby Dick",
-        authors = listOf("Herman Melville"),
-        imageLinks = imageLinks
-    )
-    BookshelfTheme {
-        BookCard(bookInfo = bookInfo, placeholder = R.drawable.moby_dick)
+fun MainScreenPreview(){
+    val bookImageUrls: ArrayList<String>  = arrayListOf()
+     for (num in 0..7){
+         bookImageUrls.add("https://example.com")
     }
+    MainScreen(bookImageUrls = bookImageUrls, placeholder = R.drawable.moby_dick)
 }
+
 
 const val PLACEHOLDER_URL = "https://fakeimg.pl/200x250?text=No+cover&font=bebas"
